5307fb29bee792adba357e3151a5a26578b23d82
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-16 22:45:46 +0700

    60 %

--- Squares_WeekOne/Source.cpp
+++ Squares_WeekOne/Source.cpp
@@ -1,10 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-double sumArea = 0;
-double width = 0;
-double legth = 0;
-char name[50];
+
 
 double AreaRectangle(double w, double l)
 {
@@ -19,34 +16,38 @@ double AreaRectangle(double w, double l)
 
 int main()
 {
-    printf(" Pleac ");
+    double sumArea = 0;
+    double width = 0;
+    double legth = 0;
+    char name[50];
+    printf(" Please specify name : ");
     scanf("%s", &name);
-    printf("Hi %s ", name);
+    printf("Hi \t %s \t welcome to Laundry112  \n", name);
 
 
-    /*
-    printf("In Put Width :");
-    if (scanf("%lf",&width))
+
+    printf("Please specify a width :");
+    if (scanf_s("%lf", &width))
     {
-      printf("In Put Length :");
-      if (scanf("%lf",&legth))
-      {
-        sumArea =  AreaRectangle(width,legth);
-        printf(" \t\t\t\t-- Ans >> AreaRectangle  = %.2lf -- \n",sumArea);
-      }
-      else
-      {
-      printf("\t\t\t ----- Please enter the number of numbers as integers. -----\n");
-      printf("\t\t\t\t\t ----- End Program. ----- \n ");
-      }
+        printf("Please specify a legth :");
+        if (scanf_s("%lf", &legth))
+        {
+            sumArea = AreaRectangle(width, legth);
+            printf(" \t\t\t\t-- Ans >> AreaRectangle  = %.2lf Square meter -- \n", sumArea);
+        }
+        else
+        {
+            printf("\t\t\t ----- Please enter the number of numbers as integers. -----\n");
+            printf("\t\t\t\t\t ----- End Program. ----- \n ");
+        }
     }
     else
     {
-      printf("\t\t\t ----- Please enter the number of numbers as integers. -----\n");
-      printf("\t\t\t\t\t ----- End Program. ----- \n ");
+        printf("\t\t\t ----- Please enter the number of numbers as integers. -----\n");
+        printf("\t\t\t\t\t ----- End Program. ----- \n ");
     }
     printf("\t\t\t\t\t ----- By Watcharapol -----\n");
     printf("\t\t\t\t----- I started to fall in love with this. -----\n");
     return 0;
-    */
+
 }
